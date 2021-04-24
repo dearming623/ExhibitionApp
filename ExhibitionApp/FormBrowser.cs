521f7754812eed21075afb7b3177f7e731fe1af8
dearming623 <t8ming@live.com>
2021-04-24 23:21:54 +0800

    更新cef库

--- ExhibitionApp/FormBrowser.cs
+++ ExhibitionApp/FormBrowser.cs
@@ -17,12 +17,129 @@ namespace ExhibitionApp
         public FormBrowser()
         {
             InitializeComponent();
-
-           
         }
 
         public delegate void onScreenClickEventHandler(object sender, EventArgs e);
         public event onScreenClickEventHandler onScreenClickEvent;
+         
+        private List<MyLink> links = null;
+        private int loc_x = 180;
+        private int loc_y = 10;
+
+        private ChromiumWebBrowser displayBrowser = null;
+        private Dictionary<string, ChromiumWebBrowser> browsers = new Dictionary<string, ChromiumWebBrowser>();
+        private CefSettings cefSettings = null;
+        private void FormBrowser_Load(object sender, EventArgs e)
+        {
+            cefSettings = new CefSettings();
+            //设置浏览器语言，默认en-us
+            cefSettings.Locale = "zh-cn";
+            //设置userAgent
+            cefSettings.UserAgent = "Mozilla/5.0(Windows NT 6.1; WOW64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+            // settings.LegacyJavascriptBindingEnabled = true;
+            if (!Cef.IsInitialized)
+            {
+                Cef.Initialize(cefSettings);
+            }
+
+            // loadData();
+            links = MyAppSetting.GetInstance().GetCompanyLinks();
+
+            loc_x = 10;
+            if (links != null && links.Count > 0)
+            {
+                for (int i = 0; i < links.Count; i++)
+                {
+                    //Button btn = new Button();
+                    //btn.BackgroundImage = ExhibitionApp.Properties.Resources.Navigation_Button2;
+                    //btn.AutoSize = true;
+
+                    string key = links[i].websiteName;
+                    string url = links[i].url;
+
+                    ImageButton tabButton = new ImageButton();
+                    tabButton.Location = new Point(loc_x, loc_y);
+                    tabButton.setFlagForNavigation(true);
+                    tabButton.Size = new Size(160, 80);
+                    tabButton.NormalImage = ExhibitionApp.Properties.Resources.Navigation_Button21;
+                    tabButton.Click += Btn_Click;
+                    tabButton.Text = links[i].websiteName;
+                    tabButton.Tag = links[i];
+                     
+                    panel1.Controls.Add(tabButton);
+
+                    loc_x += tabButton.Width ;
+
+                    ChromiumWebBrowser browser = new ChromiumWebBrowser(url); 
+                    browser.JavascriptObjectRepository.Settings.LegacyBindingEnabled = true;
+                    browser.LifeSpanHandler = new LifeSpanHandler();
+                    browsers.Add(key, browser);
+
+                    if (i == 0)
+                    {
+                        tabButton.NormalImage = ExhibitionApp.Properties.Resources.Navigation_Button11;
+                        displayBrowser = browser;
+                        BringToMain(displayBrowser);
+
+                    }
+                }
+                  
+            }
+
+            panel1.Visible = true;
+            panel2.Visible = true;
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            foreach (var item in panel1.Controls)
+            {
+                if (item.GetType() == typeof(ImageButton))
+                {
+                    ImageButton render = (ImageButton)item;
+
+                    if (render.Name == "btn_menu" ||
+                        render.Name == "btn_go_forward" ||
+                        render.Name == "btn_go_back")
+                        continue;
+
+                    render.NormalImage = ExhibitionApp.Properties.Resources.Navigation_Button21;
+                }
+            }
+
+            ImageButton tabButton = (ImageButton)sender;
+            tabButton.NormalImage = ExhibitionApp.Properties.Resources.Navigation_Button11;
+
+            MyLink link = (MyLink)tabButton.Tag;
+
+            displayBrowser = browsers[link.websiteName];
+
+            BringToMain(displayBrowser);
+            
+        }
+
+        private void BringToMain(ChromiumWebBrowser displayBrowser)
+        {
+            displayBrowser.Dock = DockStyle.Fill; 
+            this.panel2.Controls.Clear();
+            this.panel2.Controls.Add(displayBrowser);
+        }
+
+        private void onClickGoBack(object sender, EventArgs e)
+        {
+            if (displayBrowser!= null && displayBrowser.CanGoBack)
+            {
+                displayBrowser.GetBrowser().GoBack();
+            }
+        }
+
+        private void onClickGoForward(object sender, EventArgs e)
+        {
+            if (displayBrowser != null && displayBrowser.CanGoForward)
+            {
+                displayBrowser.GetBrowser().GoForward();
+            }
+        }
 
         private void menu_option_Click(object sender, EventArgs e)
         {
@@ -34,169 +151,6 @@ namespace ExhibitionApp
             if (onScreenClickEvent != null)
             {
                 onScreenClickEvent(null, null);
-            }
-        }
-
-        ChromiumWebBrowser wb = null;
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (!CefSharp.Cef.IsInitialized)
-            {
-                //必须进行初始化，否则就出来页面啦。
-                CefSharp.Cef.Initialize();
-            }
-           
-
-            //实例化控件
-            ChromiumWebBrowser wb = new ChromiumWebBrowser("http://www.baidu.com");
-            //设置停靠方式
-            wb.Dock = DockStyle.Fill;
-
-           
-
-            //加入到当前窗体中
-            //this.Controls.Add(wb);
-            this.panel2.Controls.Add(wb);
-
-            
-        }
-
-
-        class MyLifeSpanHandler : ILifeSpanHandler
-        {
-        
-            public bool OnBeforePopup(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
-            {
-                // Preserve new windows to be opened and load all popup urls in the same browser view
-                browserControl.Load(targetUrl);
-
-                newBrowser = browserControl;
-                //
-                return true;
-            }
-
-            public void OnAfterCreated(IWebBrowser browserControl, IBrowser browser)
-            {
-                //throw new NotImplementedException();
-            }
-
-            public bool DoClose(IWebBrowser browserControl, IBrowser browser)
-            {
-                //throw new NotImplementedException();
-                return true;
-            }
-
-            public void OnBeforeClose(IWebBrowser browserControl, IBrowser browser)
-            {
-                //throw new NotImplementedException();
-            }
-        }
-
-         
-
-        private List<MyLink> lst = null;
-        private int loc_x = 180;
-        private int loc_y = 10;
-
-
-        private void FormBrowser_Load(object sender, EventArgs e)
-        {
-
-            // loadData();
-            lst = MyAppSetting.GetInstance().GetCompanyLinks();
-
-            loc_x = 10;
-            if (lst != null && lst.Count >0)
-            {
-                for (int i = 0; i < lst.Count; i++)
-                {
-                    //Button btn = new Button();
-                    //btn.BackgroundImage = ExhibitionApp.Properties.Resources.Navigation_Button2;
-                    //btn.AutoSize = true;
-                    
-                    ImageButton btn_item = new ImageButton();
-                    btn_item.setFlagForNavigation(true);
-                    btn_item.Size = new Size(160, 80);
-                    btn_item.NormalImage = ExhibitionApp.Properties.Resources.Navigation_Button21;
-                    btn_item.Click += Btn_Click;
-                    btn_item.Text = lst[i].websiteName;
-                    btn_item.Tag = lst[i];
-
-                    btn_item.Location = new Point(loc_x,loc_y);
-
-                    panel1.Controls.Add(btn_item);
-
-                    loc_x += btn_item.Width ;
-
-                    if (i == 0)
-                    {
-                        wb = new ChromiumWebBrowser(lst[i].url);
-                        wb.Dock = DockStyle.Fill;
-
-                        btn_item.NormalImage = ExhibitionApp.Properties.Resources.Navigation_Button11;
-                        this.panel2.Controls.Clear();
-                        this.panel2.Controls.Add(wb);
-                    }
-                }
-                
-            }
-
-            panel1.Visible = true;
-            panel2.Visible = true;
-        }
-
-
-        private void Btn_Click(object sender, EventArgs e)
-        {
-
-            foreach (var item in  panel1.Controls)
-            {
-                if (item.GetType() == typeof(ImageButton))
-                {
-                    ImageButton render = (ImageButton)item;
-
-                  
-                    if (render.Name == "btn_menu" || 
-                        render.Name == "btn_go_forward" || 
-                        render.Name == "btn_go_back")
-                        continue;
-                    
-                     render.NormalImage = ExhibitionApp.Properties.Resources.Navigation_Button21;
-                }
-                
-            }
-
-            if (!CefSharp.Cef.IsInitialized)
-            {
-                CefSharp.Cef.Initialize();
-            }
-
-            ImageButton btn = (ImageButton)sender;
-            MyLink link = (MyLink)btn.Tag;
-
-            wb = new ChromiumWebBrowser(link.url);
-            wb.Dock = DockStyle.Fill;
-
-            wb.LifeSpanHandler = new MyLifeSpanHandler();
-            btn.NormalImage = ExhibitionApp.Properties.Resources.Navigation_Button11;
-            this.panel2.Controls.Clear();
-            this.panel2.Controls.Add(wb);
-        }
-
-        private void onClickGoBack(object sender, EventArgs e)
-        {
-            if (wb!= null && wb.CanGoBack)
-            {
-                wb.GetBrowser().GoBack();
-            }
-        }
-
-        private void onClickGoForward(object sender, EventArgs e)
-        {
-            if (wb != null && wb.CanGoForward)
-            {
-                wb.GetBrowser().GoForward();
             }
         }
     }
---
+++ ExhibitionApp/LifeSpanHandler.cs
@@ -0,0 +1,35 @@
+﻿using CefSharp;
+using System;
+
+namespace ExhibitionApp
+{ 
+    public class LifeSpanHandler : ILifeSpanHandler
+    {
+        public bool DoClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            //return true here to handle closing yourself (no WM_CLOSE will be sent).
+            //return true; 
+
+            //The default CEF behaviour (return false) will send a OS close notification (e.g. WM_CLOSE).
+            //See the doc for this method for full details.
+            return false;
+        }
+
+        public void OnAfterCreated(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnBeforeClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public bool OnBeforePopup(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            newBrowser = null;
+            chromiumWebBrowser.Load(targetUrl);
+            return true; //Return true to cancel the popup creation
+        }
+    }
+}
